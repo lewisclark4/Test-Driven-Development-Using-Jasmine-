ae0fb3a22ea749c707c9612867cdbf4b5241ddc6
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-01-10 15:23:29 +0000

    amended addition calc

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,14 +1,23 @@
 describe("Calculator", function() {
+
+    beforeEach(function() {
+    calc = new Calculator;
+    });
+
     describe("Addition function", function() {
         it("should return 42", function() {
-            expect(addition(20, 22)).toBe(42);
+            calc.add(20);
+            calc.add(22)
+            expect(calc.value).toBe(42);
         });
         it("should return 26", function() {
-            expect(addition(7, 19)).toBe(26);
+            calc.add(17);
+            calc.add(9)
+            expect(calc.value).toBe(26);
         });
         it("should return an error if we don't provide two numbers", function() {
             spyOn (window, "alert");
-            addition("Hitchhikers", "Guide");
+            calc.add("Hitchhikers");
             expect(window.alert).toHaveBeenCalledWith("Error!");
         });
     });
